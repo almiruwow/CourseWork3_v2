00f3f8a33cd216023a96ce542434ed9ad74c0950
almiruwow <101392565+almiruwow@users.noreply.github.com>
2024-03-02 20:46:06 +0300

    error correction

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from utils.functions import load_data, sort_date, search_top_transactions
+from utils.functions import load_data, sort_date, search_top_transactions, show_info
 
 
 if __name__ == '__main__':
     data = load_data()
-
     actual_transactions, data_transactions = sort_date(data)
-    print(len(search_top_transactions(actual_transactions, data_transactions)))
+    info = search_top_transactions(actual_transactions, data_transactions)
+    print(show_info(info))
+    
 
-    for d in search_top_transactions(actual_transactions, data_transactions):
-        print(d)
+    
